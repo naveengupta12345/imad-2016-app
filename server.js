9b3524bfeeb653ef431477b8f3940eeacd0f5f35
naveengupta12345 <ernaveengupta1997@gmail.com>
2016-10-12 21:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'article-one.html*'));
+  res.sendFile(path.join(__dirname, 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('This is article two serving you.....WElcome!!!');
