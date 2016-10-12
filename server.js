09aaec3f918696ec2f6ec0cdddc2c3afcd47fab6
naveengupta12345 <ernaveengupta1997@gmail.com>
2016-10-12 17:03:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/article-one', function (req, res) {
+  res.send('This is article one serving you.....WElcome!!!');
+});
+app.get('/article-two', function (req, res) {
+  res.send('This is article two serving you.....WElcome!!!');
+});
+app.get('/article-three', function (req, res) {
+  res.send('This is article three serving you.....WElcome!!!');
 });
 
 app.get('/ui/madi.png', function (req, res) {
